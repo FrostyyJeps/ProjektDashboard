8698070d04fe2bcd665b077d3497e407941138ec
Halfdan <woody7maddox@yahoo.dk>
2020-08-31 10:52:39 +0200

    Basic HTML crap - sourcemaps/autoprefixer/postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 const gulp = require("gulp")
 const sass = require("gulp-sass")
+const sourcemaps = require("gulp-sourcemaps")
+const postcss = require("gulp-postcss")
+const autoprefixer = require("autoprefixer")
 
 function styles(){
 
     return(
         gulp.src("css/*.scss")
+            .pipe(sourcemaps.init())
             .pipe(sass())
+            .pipe(postcss([autoprefixer()]))
+            .pipe(sourcemaps.write("."))
             .pipe(gulp.dest("css"))
     );
 
